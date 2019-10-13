1806428b7badb0276e07523af5289e122b5bd893
Paul Shen <pxshen@alumni.stanford.edu>
2019-10-13 19:29:59 -0400

    some message

--- pip.bat
+++ pip.bat
@@ -1,3 +1,8 @@
+cd C:\Users\Paul\Desktop\gpt2server\openmedicalio
+rmdir /q /s build
+rmdir /q /s openmedicalio.egg-info
+rmdir /q /s dist
+
 python -m pip install --user --upgrade setuptools wheel
 python setup.py sdist bdist_wheel
 python -m pip install --user --upgrade twine
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="openmedicalio",
-    version="0.0.4",
+    version="0.0.5",
     author="Open Medical IO",
     author_email="info@openmedical.io",
     description="API client for natural language processing tools by Open Medical IO",
